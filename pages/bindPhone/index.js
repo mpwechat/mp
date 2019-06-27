259f4bc78e4cf9a017813434892a6f306cab91f6
name <yangxianfei@supcon.com>
2019-06-27 19:26:18 +0800

    a

--- pages/bindPhone/index.js
+++ pages/bindPhone/index.js
@@ -7,7 +7,7 @@ Page({
   data: {
     countdownShow:true,
     countdown:60,
-    phoneNumber:'15709613629',
+    phoneNumber:'17326033529',
     verification:1
   },
   inputChange: function (e) {
@@ -44,14 +44,30 @@ let that=this;
      dataType: 'json',
      responseType: 'text',
      success: function(res) {
-       debugger
-       let pages = wx.getStorageSync('router')
-       wx.setStorageSync("sessionid", res.header["Set-Cookie"])
-       wx.redirectTo({
-         url: pages  //或者url: '/page/person/goldcoin/index'
-       })
+       var pagesSrc = wx.getStorageSync('router')
+       wx.setStorageSync("sessionid", res.header["Set-Cookie"]);
+       switch (pagesSrc){
+         case'/pages/order/index':
+           wx.switchTab({
+             url: '/pages/order/index',
+           })
+         break;
+         case '/pages/userCenter/index':
+           wx.switchTab({
+             url: '/pages/userCenter/index',
+           })
+         break;
+         default:
+           wx.redirectTo({
+             url: pagesSrc //或者url: '/page/person/goldcoin/index'
+           })
+
+       }
+      
      },
-     fail: function(res) {},
+     fail: function(res) {
+      
+     },
      complete: function(res) {},
    })
   },
--- pages/payment/index.js
+++ pages/payment/index.js
@@ -1,4 +1,6 @@
 // pages/paySuccess/index.js
+const md5 = require('../../utils/md5.js');
+const moment = require('../../utils/moment.js')
 Page({
 
   /**
@@ -11,7 +13,8 @@ Page({
       { text: '支付宝', img:'../../asset/images/payment_ali.png',check:false,index:0},
       { text: '微信支付', img: '../../asset/images/payment_wechat.png', check: false, index: 1},
       { text: '银联支付', img: '../../asset/images/payment_card.png', check: false, index:2 },
-    ]
+    ],
+    prciePay:0.1,
    
   },
 
@@ -47,6 +50,51 @@ var that=this;
     this.setData({
       paymentType: this.data.paymentType
     })
+  },
+  /**
+   * 支付
+   */
+  pay(){
+    console.log(Date.parse(new Date()))
+    debugger
+    var timeStamp = this.timeStamp();
+    var randomString = this.randomString();
+    var Mixedicymd5 = this.MixedencryMD5(res_paydata, randomString, timeStamp) 
+    var paySign = md5.hexMD5(Mixedicymd5 )
+    wx.requestPayment(
+      {
+        'timeStamp': timeStamp,
+        'nonceStr': randomString,
+        'package': 'prepay_id ='+ wx2017033010242291fcfe0db70013231072,
+        'signType': 'MD5',
+        'paySign': paySign,
+        'success': function (res) { 
+
+        },
+        'fail': function (res) { },
+        'complete': function (res) { }
+      }) 
+
+
+
+  },
+  // 调起支付签名
+ MixedencryMD5(res_paydata, randomString, timeStamp) {
+    return "appId=" + config.appid + "&nonceStr=" + randomString + "&package=prepay_id=" + res_paydata + "&signType=MD5" + "&timeStamp=" + timeStamp + "&key=" + config.key;
+  },
+  // / 时间戳
+ timeStamp() {
+    return parseInt(new Date().getTime() / 1000) + ''
+  },
+/* 随机数 */
+randomString() {
+    var chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';    /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
+    var maxPos = chars.length;
+    var pwd = '';
+    for (var i = 0; i < 32; i++) {
+      pwd += chars.charAt(Math.floor(Math.random() * maxPos));
+    }
+    return pwd;
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
